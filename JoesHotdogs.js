10c91bac4843d1f0e1df1775d576b670233d3b14
Matthew Sims <32784225+Optidox@users.noreply.github.com>
2021-10-10 23:43:02 -0500

    joes hotdogs 2

--- JoesHotdogs.js
+++ JoesHotdogs.js
@@ -7,23 +7,38 @@ function hotdogCalc() {
     const tax = .0625 * (subtotal - discount);
     const total = subtotal - discount + tax;
 
-    document.write( `<div style="display:inline-block; padding=16px;">` +
-                        `<p>Hotdogs:&nbsp;</p>` +
-                        `<p>French Fries:&nbsp;</p>` +
-                        `<p>Drinks:&nbsp;</p>` +
-                        `<p>Subtotal:&nbsp;</p>` +
-                        `<p>Discount:&nbsp;</p>` +
-                        `<p>Tax:&nbsp;</p>` +
-                        `<p>Total:&nbsp;</p>` +
-                    `</div>` +
-                    `<div style="display:inline-block;">` +
-                        `<p>${hotdogs}</p>` +
-                        `<p>${frenchfries}</p>` +
-                        `<p>${drinks}</p>` +
-                        `<p>$${subtotal}</p>` +
-                        `<p>$${discount.toFixed(2)}</p>` +
-                        `<p>$${tax.toFixed(2)}</p>` + 
-                        `<p>$${total.toFixed(2)}</p>` +
-                    `</div>`);
+    document.write( 
+                    `<style>
+                        #col1 { 
+                            display: inline-block;
+                            padding: 16px;
+                            color: blue;
+                            font-size: 24px
+                        }
+                        #col2 {
+                            display: inline-block;
+                            padding: 16px;
+                            color: red;
+                            font-size: 24px
+                        }
+                    </style>
+                    <div id=col1>
+                        <p>Hotdogs:&nbsp;</p> 
+                        <p>French Fries:&nbsp;</p>
+                        <p>Drinks:&nbsp;</p>
+                        <p>Subtotal:&nbsp;</p>
+                        <p>Discount:&nbsp;</p>
+                        <p>Tax:&nbsp;</p>
+                        <p>Total:&nbsp;</p>
+                    </div>
+                    <div id=col2>
+                        <p>${hotdogs}</p>
+                        <p>${frenchfries}</p>
+                        <p>${drinks}</p>
+                        <p>$${subtotal}</p>
+                        <p>$${discount.toFixed(2)}</p>
+                        <p>$${tax.toFixed(2)}</p>
+                        <p>$${total.toFixed(2)}</p>
+                    </div>`);
     document.close();
 }
